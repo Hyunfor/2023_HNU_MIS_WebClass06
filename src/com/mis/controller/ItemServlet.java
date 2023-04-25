2e6817a61f4a4d4e42cd064a2055bd523cfbbf5a
Hyunfor <navel5148@gmail.com>
2023-04-25 09:01:58 +0900

    퀴즈 주석문 변경

--- src/com/mis/controller/ItemServlet.java
+++ src/com/mis/controller/ItemServlet.java
@@ -48,13 +48,13 @@ public class ItemServlet extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 
-		 // 1. 요청 파라미터에서 로그인 정보 추출
+		 // 1. 요청 파라미터에서 아이템 입력 정보 추출
         String name = request.getParameter("name");
         int price = Integer.parseInt(request.getParameter("price"));
         String description = request.getParameter("description");
         String user = request.getParameter("user");
 
-        // 2. MemberBean 객체 생성 및 로그인 정보 저장
+        // 2. MemberBean 객체 생성 및 아이템 정보 저장
         ItemBean item = new ItemBean();
         item.setName(name);
         item.setPrice(price);
@@ -62,11 +62,11 @@ public class ItemServlet extends HttpServlet {
         item.setUser(user);
 
 
-        // 3. 세션에 로그인 정보 저장
+        // 3. 세션에 아이템 정보 저장
         HttpSession session = request.getSession();
         session.setAttribute("item", item);
 
-        // 4. main.jsp 페이지로 이동
+        // 4. iteminfo.jsp 페이지로 이동
         RequestDispatcher dispatcher = request.getRequestDispatcher("iteminfo.jsp");
         dispatcher.forward(request, response);
 	}
